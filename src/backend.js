741b8d5765382c6f075dd7a0f51b0d101b100cc0
nbhat1998 <nbhat1998@gmail.com>
2019-06-01 20:46:42 +0100

    implemented imgur api, changed sql query accordingly

--- src/backend.js
+++ src/backend.js
@@ -1,77 +1,102 @@
-const express = require('express')
-var bodyparser = require('body-parser')
-var cors = require('cors')
-const app = express()
+const express = require("express");
+var bodyparser = require("body-parser");
+var cors = require("cors");
+const app = express();
 
 var corsOptions = {
-  origin: '*',
+  origin: "*",
   optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({
-  extended: true
-}))
+app.use(bodyparser.json());
+app.use(
+  bodyparser.urlencoded({
+    extended: true
+  })
+);
 
-app.set('port', process.env.PORT || 8080)
-
+app.set("port", process.env.PORT || 8080);
 
 // DATABASE
-var pgp = require('pg-promise')();
+var pgp = require("pg-promise")();
 
 const db = pgp(process.env.DATABASE_URL);
 
 app.get("/", (req, res) => {
-  console.log("Backend running on port " + app.get('port'))
-  res.send({PORT : app.get('port')})
-}
-)
+  console.log("Backend running on port " + app.get("port"));
+  res.send({ PORT: app.get("port") });
+});
 
 app.get("/notice/:column", (req, res) => {
-  console.log("Request Started")
-  db.any('SELECT ' + req.params.column + ' FROM notice ORDER BY id DESC')
-    .then(function (data) {
+  console.log("Request Started");
+  db.any("SELECT " + req.params.column + " FROM notice ORDER BY id DESC")
+    .then(function(data) {
       res.send({
         DATA: data
-      })
+      });
     })
-    .catch((err) => {
-      console.log(err)
-      res.send("500")
-    })
-})
-
+    .catch(err => {
+      console.log(err);
+      res.send("500");
+    });
+});
 
 app.get("/notice/max/:column", (req, res) => {
-  console.log("Request Started")
-  db.any('SELECT max(' + req.params.column + ') FROM notice')
-    .then(function (data) {
+  console.log("Request Started");
+  db.any("SELECT max(" + req.params.column + ") FROM notice")
+    .then(function(data) {
       res.send({
         DATA: data
-      })
+      });
     })
-    .catch((err) => {
-      console.log(err)
-      res.send("500")
-    })
-})
-
+    .catch(err => {
+      console.log(err);
+      res.send("500");
+    });
+});
 
 //post example: curl -d "id=69&title="test"&description="a"&community="a"&tags="ghsj"" -X POST localhost:8080/submit
 app.post("/submit", (req, res) => {
-  console.log("Submission receieved for", req.body.id, req.body.title, req.body.description, req.body.community, req.body.tags)
-  db.any("INSERT INTO notice (id, title, description, community, tags, pic1) VALUES (" +
-      req.body.id + ", \'" + req.body.title + "\', \'" + req.body.description + "\', \'" + req.body.community + "\', \'" + req.body.tags + "\', \'" + req.body.pic1 + "\')" )
-    .then(function (data) {
-      res.send("200")
+  console.log(
+    "Submission receieved for",
+    req.body.id,
+    req.body.title,
+    req.body.description,
+    req.body.community,
+    req.body.tags,
+    req.body.pic1,
+    req.body.pic2,
+    req.body.pic3
+  );
+  db.any(
+    "INSERT INTO notice (id, title, description, community, tags, pic1, pic2, pic3) VALUES (" +
+      req.body.id +
+      ", '" +
+      req.body.title +
+      "', '" +
+      req.body.description +
+      "', '" +
+      req.body.community +
+      "', '" +
+      req.body.tags +
+      "', '" +
+      req.body.pic1 +
+      "', '" +
+      req.body.pic2 +
+      "', '" +
+      req.body.pic3 +
+      "')"
+  )
+    .then(function(data) {
+      res.send("200");
     })
-    .catch((err) => {
-      console.log(err)
-      res.send("500")
-    })
-})
+    .catch(err => {
+      console.log(err);
+      res.send("500");
+    });
+});
 
-app.listen(app.get('port'), function () {
-  console.log('Server listening on port ' + app.get('port'))
-})
+app.listen(app.get("port"), function() {
+  console.log("Server listening on port " + app.get("port"));
+});
