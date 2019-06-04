00a45bd87226bc5e91c3059beb155a1532eb1c55
Bianca-Alexandra Casapu <bianca-alexandra.casapu17@imperial.ac.uk>
2019-06-04 19:26:21 +0100

    Rephrasing

--- src/backend.test.js
+++ src/backend.test.js
@@ -3,8 +3,8 @@ const backend = require('./backend')
 const request = require("supertest");
 const db = require("./db");
 
-describe("GET / ", () => {
-  test("Index should respond with something", async () => {
+describe("TEST: GET / ", () => {
+  test("Index should respond", async () => {
     const response = await request(backend).get("/");
     expect(response.body).toBeDefined();
     expect(response.statusCode).toBe(200);
